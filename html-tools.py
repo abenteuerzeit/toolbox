e6c42bed5a8e7fa99ca10e5a207b15451d124849
abenteuerzeit <amroz.muzyka@gmail.com>
2022-04-26 08:19:03 +0200

    Add tag to function

--- html-tools.py
+++ html-tools.py
@@ -1,13 +1,17 @@
-def remove_html_tag_attributes(file, attributes_list):
-    # Remove selected attributes from an HTML tag
+def remove_html_tag_attributes(file, attributes_list, tag):
+    # Remove selected attributes from the HTML tag
+    # Write True for tag to remove attributes from all tags
     # A new file is generated
     updated_list = [' ' + attribute.lower() + '="' for attribute in attributes_list]
     with open(file) as html_file:
         new_lines = []
         for line in html_file.readlines():
-            line = line
             for attribute in updated_list:
-                while attribute in line.lower():
+                is_tag = line.strip()
+                if is_tag != '':
+                    tag_idx = line.find(tag)
+                    is_tag = f"<{line[tag_idx]}" in is_tag[0:]
+                while is_tag and attribute in line.lower():
                     start = line.find(attribute)
                     if line.find(';'):
                         stop = line.find(';') + 1
@@ -20,7 +24,8 @@ def remove_html_tag_attributes(file, attributes_list):
 
         file_name = file[:file.find('.html')]
         capitalize = [attribute.upper()[0] + attribute.lower()[1:] for attribute in attributes_list]
-        renamed_file = file_name + f"_{'-'.join(capitalize)}" + "-removed" + ".html"
+        tag = tag if type(tag) is str else "all"
+        renamed_file = file_name + f"_{'-'.join(capitalize)}" + f"-removed-from-{tag}" + ".html"
         save_new_file = open(renamed_file, 'w')
         save_new_file.writelines(new_lines)
 
@@ -54,5 +59,5 @@ if __name__ == "__main__":
     # for file in files:
     #     filepath = f"./templates/{file}"
     #     remove_html_tag_attributes(filepath, ["style", "class"])
-    remove_tag('add-question.html', 'a')
+    # remove_tag('add-question.html', 'a')
     pass
